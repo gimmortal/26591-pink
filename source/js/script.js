37ea5d7e7bbad41589b5efba6b896dcd84284e31
gimmortal <ildusgallymov@mail.ru>
2015-10-26 16:13:44 +0500

    update

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,33 @@
+$(document).ready(function() {
+
+  var owl = $("#owl-pink");
+
+  owl.owlCarousel({
+    items : 3, //10 items above 1000px browser width
+    itemsDesktop : [1000,5], //5 items between 1000px and 901px
+    //navigationText: false,
+    pagination: false,
+    navigation: false, // Show next and prev buttons
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    singleItem: true
+
+  });
+
+  $(".next").click(function(){
+    owl.trigger('owl.next');
+  })
+  $(".prev").click(function(){
+    owl.trigger('owl.prev');
+  })
+  $(".play").click(function(){
+    owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+  })
+  $(".stop").click(function(){
+    owl.trigger('owl.stop');
+  })
+});
+
 var link = document.querySelector('.page-header-menu__open-btn');
 var popup = document.querySelector('.page-header-menu__popup');
 var close = popup.querySelector('.page-header-menu__popup-close');
@@ -93,3 +123,4 @@ for (var i = 0; i < elements.length; i++) {
 
   }
 })();
+
